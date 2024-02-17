eb3d5e6cf6a1e89d80f544486a1ee61f23b22324
Najam <hmnajam@gmail.com>
2024-02-17 19:41:15 +0500

    Added messages in dashboard sidebar

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  ChatBubbleLeftRightIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -19,6 +20,11 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  {
+    name: 'Messages',
+    href: '/dashboard/messages',
+    icon: ChatBubbleLeftRightIcon,
+  },
 ];
 
 export default function NavLinks() {
