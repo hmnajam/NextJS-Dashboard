ab2b77c4de034e7e13f73678a92f33d46bab9c97
Najam <hmnajam@gmail.com>
2024-02-18 22:27:52 +0500

    Showing messages in messages page

--- app/dashboard/messages/page.tsx
+++ app/dashboard/messages/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function Page() {
-  return <p>Messages Page</p>;
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://express-hello-world-iv2o.onrender.com/get-all-messages',
+        );
+        const data = await response.json();
+        setMessages(data.response);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {/* Render your component using the 'messages' state */}
+      {messages.map((msg, index) => (
+        <div key={index}>
+          <p>Recipient: {msg.recipient}</p>
+          <p>Message: {msg.message}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
