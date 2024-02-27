e54cdcd71666381f528c7579929cc7556e8b698d
Najam <hmnajam@gmail.com>
2024-02-27 23:33:09 +0500

    changed get-all-messages to Chatify-LabCloud

--- app/dashboard/messages/page.tsx
+++ app/dashboard/messages/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          'https://chatify-whxk.onrender.com/get-all-messages',
+          'https://chatterly-labcloud-4a8k.onrender.com/get-all-messages',
         );
         const data = await response.json();
         setMessages(data.response);
